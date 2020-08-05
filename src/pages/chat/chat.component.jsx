295ebcb50bf32206abf4d42ac89b112b18bd136c
salman <ssalmanyo9@gmail.com>
2020-08-05 16:12:16 +0530

    Final ui for chat

--- src/pages/chat/chat.component.jsx
+++ src/pages/chat/chat.component.jsx
@@ -5,13 +5,15 @@ import { connect } from 'react-redux';
 import { getStudentByClass, getSTeacher } from '../../firebase/firebase.utils';
 import {ReactComponent as Sbox} from '../../assets/sb.svg';
 import {ReactComponent as Userp} from '../../assets/up.svg';
+import {ReactComponent as Send} from '../../assets/send.svg';
 
 class Chat extends Component{
     constructor(props){
         super(props);
         this.state={
             students:[],
-            teacher:{}
+            teacher:{},
+            name:''
         }
     }
     componentDidMount=async()=>{
@@ -21,8 +23,20 @@ class Chat extends Component{
             this.setState({students:await getStudentByClass(this.props.currentUser.Class)})
         }
         else if(this.props.currentUser.status==='student'){
-            this.setState({teacher: await getSTeacher(this.props.currentUser.Class)},()=>{console.log(this.state)})
+            this.setState({teacher: await getSTeacher(this.props.currentUser.Class)})
         }
+    }
+    componentWillUnmount(){
+        var msg = document.querySelector('.msg')
+        msg.classList.toggle("open");
+    }
+
+    setHeader = (person) =>{
+        if(this.state.name===''){
+        var chatHeader = document.querySelector('.chat-header')
+        chatHeader.classList.toggle("show");
+        }
+        this.setState({name:person.displayName})
     }
     render(){
         return(
@@ -33,19 +47,29 @@ class Chat extends Component{
                     <Sbox className='s-b'/>
                 </div>
                 {this.state.students?this.state.students.map(student=>(
-                    <div className='chat-card' key={student.admissionNo}>
+                    <div className='chat-card' key={student.admissionNo}
+                    onClick={()=>{this.setHeader(student)}}
+                    >
                         
                         <Userp className='u-p'/>
                         <div className='chat-name'>{student.displayName}</div>
                         
                     </div>
                 )):null}
-                {Object.keys(this.state.teacher).length!==0?<div className='chat-card'>
+                {Object.keys(this.state.teacher).length!==0?<div className='chat-card'   onClick={()=>{this.setHeader(this.state.teacher)}}>
                         <Userp className='u-p'/>
                          <div className='chat-name'>{this.state.teacher.displayName}</div>
                          </div>
                 :null
                 }
+            </div>
+            <div className='chat-header'>
+                <Userp className='u-p'/>
+                <div className='chat-name'>{this.state.name}</div>
+            </div>
+            <div className='chat-zone'>
+                <input id='chat-z'></input>
+                <Send className='send'/>
             </div>
         </div>
         )
